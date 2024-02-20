f9bf334870887d864474ee8416f404d3001c3e34
abubakrali10 <bakrali175@yahoo.com>
2024-02-20 06:48:01 +0200

    determine the fewest number of coins needed to meet a given amount

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""make change problem"""
+
+def makeChange(coins, total):
+    """determine the fewest number of coins needed to meet a given amount of coins"""
+    if total <= 0:
+        return 0
+
+    min_coins = [float('inf')] * (total + 1)
+
+    min_coins[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            min_coins[amount] = min(min_coins[amount], min_coins[amount - coin] + 1)
+
+    return min_coins[total] if min_coins[total] != float('inf') else -1
